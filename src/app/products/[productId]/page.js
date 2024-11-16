53c13e8dce6eca410d471248c6068cf8293c000f
sina-tkm <torkmnsina@gmail.com>
2024-11-16 21:31:47 +0330

    route practice in next js

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,7 +1,6 @@
-export default function ProductDetail({ params }) {
-  // Access the productId from the params object
-  const { productId } = params;
-  console.log(productId);
-
+export default async function ProductDetail({ params }) {
+  const { productId } = await params; // Await the params object
+  console.log(productId); // Now you should get the actual productId
+  
   return <div>Product ID page: {productId}</div>;
 }
